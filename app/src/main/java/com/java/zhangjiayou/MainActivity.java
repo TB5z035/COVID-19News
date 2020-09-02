66d441e2ad4bd1bdb4fa8f0a58040e399b7359d8
TB5zhh <654154090@qq.com>
2020-09-02 17:28:56 +0800

    a little test

--- app/src/main/java/com/java/zhangjiayou/MainActivity.java
+++ app/src/main/java/com/java/zhangjiayou/MainActivity.java
@@ -2,15 +2,17 @@ package com.java.zhangjiayou;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity
+        implements SwipeRefreshLayout.OnRefreshListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+    }
+
+    @Override
+    public void onRefresh() {
 
     }
 }
